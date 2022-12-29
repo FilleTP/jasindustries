c7e0fc378cfefaa0be2b9ff1b4dd0b2676799784
Filip Persson <filip.tillberg.persson@gmail.com>
2022-12-29 17:37:23 +0100

    added video for posts

---
+++ config/initializers/html_sanitization.rb
@@ -0,0 +1,6 @@
+Rails::Html::WhiteListSanitizer.allowed_tags << "video"
+Rails::Html::WhiteListSanitizer.allowed_tags << "source"
+Rails::Html::WhiteListSanitizer.allowed_attributes << "controls"
+Rails::Html::WhiteListSanitizer.allowed_attributes << "autoplay"
+Rails::Html::WhiteListSanitizer.allowed_attributes << "controls"
+Rails::Html::WhiteListSanitizer.allowed_attributes << "muted"
