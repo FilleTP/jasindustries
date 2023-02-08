c0fe63de37516fe4ccdea5bd503745f99c20ee03
Filip Persson <filip.tillberg.persson@gmail.com>
2023-02-08 11:35:32 +0100

    added carousel in jas cares and media center

--- app/controllers/media_centers_controller.rb
+++ app/controllers/media_centers_controller.rb
@@ -11,16 +11,21 @@ class MediaCentersController < ApplicationController
 
   def create
     @media_center = MediaCenter.new(media_center_params)
-    if @media_center.save
-      redirect_to media_center_path(@media_center)
+    unless @media_center.photos.empty?
+      if @media_center.save
+        redirect_to media_center_path(@media_center)
+      else
+        render :new
+      end
     else
-      render :new
+      render :new, notice: "You must upload at least 1 image"
     end
   end
 
   def show
     @media_centers = MediaCenter.all
     @media_center = MediaCenter.with_rich_text_content_and_embeds.find(params[:id])
+    @photos = @media_center.photos.drop(1)
   end
 
   def edit
@@ -50,6 +55,6 @@ class MediaCentersController < ApplicationController
   private
 
   def media_center_params
-    params.require(:media_center).permit(:title, :content, :photo)
+    params.require(:media_center).permit(:title, :content, photos: [])
   end
 end
